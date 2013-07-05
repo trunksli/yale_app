d02cf260d9f0d135dfab5bf3584bd9e8dd2db24b
Trunks Li <trunksli@hotmail.com>
2013-07-05 17:53:57 -0400

    initial commit for yale1on1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YaleApp::Application.config.secret_token = '83769b17d74c6bb5db5bcb6e13f1fd41fc1c3ef3a23238d01444fb5f942b1a9e4e9965efedd6e0e7adf2c806046ea42635dbc043873f8568f9340b1cf6d08478'
